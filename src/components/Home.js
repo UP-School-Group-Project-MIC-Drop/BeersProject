9dbfe258d1658133c8c4d7edddca8cea27a4c34c
Cagla <caglaeyubog@gmail.com>
2022-01-07 11:45:56 +0300

    pagination component recreated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,16 @@
 import React, {useEffect, useState} from 'react'
-import ReactPaginate from 'react-paginate'
 import "../App.css"
+import Pagination from './Pagination'
 
 function Home() {
+    //states for pagination
     const [data, setData] = useState([])
     const [loading, setLoading] = useState(true)
     const perPage = 6
     const [pageCount, setPageCount] = useState(0)
     const [selectedPage, setSelectedPage] = useState(0)
 
-
-
+    //fetch api for pagination
     useEffect(() => {
         fetch("https://api.punkapi.com/v2/beers?page=1&per_page=60")
             .then(response => response.json())
@@ -21,47 +21,9 @@ function Home() {
             });
     }, [])
 
-    function handlePageClick(e) {
-        setSelectedPage(e.selected*perPage)
-    }
-    if(loading) return <h1>Loading...</h1>
 
     return (
-        <div className='container'>
-            {/* deneme12345 */}
-            asldaşkajsdk
-            <label htmlFor="">Cagla deneme</label>
-            <input type="text" />
-            <div className="row text-center">
-            {
-                data.slice(selectedPage,selectedPage + perPage).map(item => <div  key={item.id} className="col-lg-4 col-sm-6 card-group">
-                    <div className="card my-3 border border-warning py-2 h-">
-                    <img src={item.image_url} className="card-img-top card-img-size" alt="..."/>
-                    <div className="card-body ">
-                    <h5 className="card-title">{item.name}</h5>
-                    <p className="card-text">First Brewed In: {item.first_brewed}</p>
-                    </div>
-                </div>
-                </div> )
-            }
-            </div>
-            
-           
-            <ReactPaginate
-                breakLabel="..."
-                nextLabel=" >>"
-                onPageChange={handlePageClick}
-                pageRangeDisplayed={3}
-                pageCount={pageCount}
-                previousLabel="<< "
-                containerClassName={"list-unstyled d-flex align-items-center justify-content-center"}
-                previousLinkClassName={"text-warning fw-bold text-decoration-none mx-2 fs-5"}
-                nextLinkClassName={"text-warning fw-bold text-decoration-none mx-2 fs-5"}
-                disabledLinkClassName={"text-muted"}
-                pageLinkClassName={"text-decoration-none mx-1 btn btn-outline-secondary"}
-                activeLinkClassName={"fw-bold btn-outline-warning"}
-            />
-        </div>
+        <Pagination data={data} loading={loading} pageCount={pageCount} selectedPage={selectedPage} setSelectedPage={setSelectedPage} perPage={perPage} />
     )
 }
 
